14888e7c658357b5fbae2a63a8552e866421b408
Liam Nichol <Liam.Nichol@gmx.com>
2024-08-04 18:45:30 +0100

    Added and configured the application to use Entity Framework Core for interacting with a SQL Server database

---
+++ BethanysPieShopOnline/Models/BethanysPieShopDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShopOnline.Models
+{
+    public class BethanysPieShopDbContext : DbContext
+    {
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base (options) { }
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
