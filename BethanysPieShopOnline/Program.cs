f3f5ba608e02b06be9175852f9d2dee065bc45a2
Liam Nichol <Liam.Nichol@gmx.com>
2024-10-10 15:53:20 +0100

    Added explicit route mapping

--- BethanysPieShopOnline/Program.cs
+++ BethanysPieShopOnline/Program.cs
@@ -30,6 +30,10 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
 }
 
-app.MapDefaultControllerRoute();
+//app.MapDefaultControllerRoute();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 DbInitialiser.Seed(app);
 app.Run();
