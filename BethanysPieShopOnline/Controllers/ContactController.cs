bb336bcadaa139bbb4e13d5a225127dd4fb426e8
Liam Nichol <Liam.Nichol@gmx.com>
2024-10-21 15:40:21 +0100

    Implemented custom EmailTagHelper

---
+++ BethanysPieShopOnline/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShopOnline.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
