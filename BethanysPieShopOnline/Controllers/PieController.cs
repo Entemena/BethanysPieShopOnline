660924d218b1dc9869d0b3b0d0a801468484f3f2
Liam Nichol <Liam.Nichol@gmx.com>
2025-01-15 16:00:13 +0000

    Added search page

--- BethanysPieShopOnline/Controllers/PieController.cs
+++ BethanysPieShopOnline/Controllers/PieController.cs
@@ -53,5 +53,9 @@ namespace BethanysPieShopOnline.Controllers
 
             return View(pie);
         }
+        public IActionResult Search()
+        { 
+            return View(); 
+        }
     }
 }
